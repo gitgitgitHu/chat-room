ee60ff18e5b8e01eae128a9d537e21cf94fb2e7f
zhangxiaofei <xiaofei.zhang@100credit.com>
2019-03-05 17:45:47 +0800

    查看我的表情包

--- chatroom/js/home.js
+++ chatroom/js/home.js
@@ -34,33 +34,40 @@ ChatRoom.prototype = {
         this.socket.on('newMsg', function(user, msg) {//接收到新消息
             that.receivesMsg(user, msg);
         });
-        document.getElementById('saveName').addEventListener('click', function() {//确定名字
-            let userName = document.getElementById('userName').value;
-            if (userName != '') {
-                //如果输入了名字，则发起一个注册事件并将输入的昵称储存
+        $('#saveName').click(function() {
+            let userName = $('#userName').val();
+            if (userName != '') { //如果输入了名字，则发起一个注册事件并将输入的昵称储存
                 that.socket.emit('login', userName);
             } else {
                 alert('是不是忘了输名字了?');
             };
-        }, false);
-        document.getElementById('sendMsg').addEventListener('click', function() {//点击发送
-            let msgInp = document.getElementById('msgInp');
-            let msg = msgInp.value;
-            msgInp.value = '';
+        })
+        $('#sendMsg').click(function() {//点击发送
+            let msg = $('#msgInp').val();
+            $('#msgInp').val('');
             if (msg != '') {
                 that.socket.emit('sendMsg', msg); //把消息发送到服务器
                 that.receivesMsg('me', msg); //把发送的消息显示到聊天窗
             };
-        }, false);
-        document.getElementById('msgInp').addEventListener('keyup', function(e) {
-            let msgInp = document.getElementById('msgInp');
-            let msg = msgInp.value;
+        })
+        $('#msgInp').keyup(function() {
+            let msg = $('#msgInp').val();
             if (e.keyCode == 13 && msg != '') {//enter键发送消息
-                msgInp.value = '';
+                $('#msgInp').val('');
                 that.socket.emit('sendMsg', msg);
                 that.receivesMsg('me', msg);
             };
-        }, false);
+        })
+        $('#openEmoji').popover({
+            title: '我的表情',
+            placement: 'top',
+            content: "<div id='emojiList'></div>",
+            html: true,
+            trigger: 'focus'
+        });
+        $('#openEmoji').click(function () {
+            that.loadEmoji();
+        });
     },
     receivesMsg: function (user, msg) {//接收消息
         let msglist = document.getElementById('main');
@@ -69,5 +76,18 @@ ChatRoom.prototype = {
         getmsg.innerHTML = `<h5>${user}<span>${date}</span></h5><li class="list-group-item ${user != 'me' && 'list-group-item-info'}">${msg}</li>`;
         msglist.appendChild(getmsg);
         msglist.scrollTop = msglist.scrollHeight;
+    },
+    loadEmoji: function () {//加载图片
+        let box = document.createDocumentFragment();
+        for (let i = 9; i > 0; i--) {
+            let emojis = document.createElement('img');
+            emojis.src = '../emoji/' + i + '.jpg';
+            emojis.title = i;
+            box.appendChild(emojis);
+        };
+        $('#emojiList').append(box);
+        $('#emojiList img').click(function (e) {
+            $('#msgInp').val('/:' + e.target.title);
+        })
     }
 };
